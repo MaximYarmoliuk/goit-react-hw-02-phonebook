a77ec8296770c9fc001e96c7237c0f7d0713bd35
Maxim <jarmoluk97@gmail.com>
2020-02-05 16:45:56 +0200

    Updates

---
+++ precache-manifest.b90f9eba8245727467ccda339d24b698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa7be0bf911ffed83961ff20b7d1a94",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "0ae8b948f651c74a3981",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.b607452e.chunk.css"
+  },
+  {
+    "revision": "b9f2d5c81ede2cc60c51",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.56fc83c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.56fc83c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ae8b948f651c74a3981",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.80cfda8b.chunk.js"
+  },
+  {
+    "revision": "5eea2426daa3643f03a9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.6819ab10.js"
+  }
+]);
